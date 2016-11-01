294b9d59da8758227053d8576481a7d4aa60c31a
dhaberst <daniel.haberstock1@gmail.com>
2016-10-31 18:17:46 -0600

    Added episode function/Changed imports

--- Assn1/randomPolicy.py
+++ Assn1/randomPolicy.py
@@ -1,11 +1,18 @@
 import blackjack
-from pylab import *
+import numpy as np
 
 def run(numEpisodes):
     returnSum = 0.0
     for episodeNum in range(numEpisodes):
-        G = 0
-        ...
+        G = episode(0)
         print("Episode: ", episodeNum, "Return: ", G)
         returnSum = returnSum + G
     return returnSum/numEpisodes
+
+
+def episode(G):
+    initialState = blackjack.init()
+    return G
+
+
+run(10)
