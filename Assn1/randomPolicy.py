4a0a5ae6336d07a0369283744d9766b4a7c91fb0
Conner D <connerdunn7399@gmail.com>
2016-10-31 18:35:36 -0600

    This is a rough draft of equiprobable-random policy

--- Assn1/randomPolicy.py
+++ Assn1/randomPolicy.py
@@ -11,8 +11,18 @@ def run(numEpisodes):
 
 
 def episode(G):
-    initialState = blackjack.init()
-    return G
+    currentState = blackjack.init() # returns the initial state
+    while(True):
+        (reward, currentState) = blackjack.sample(currentState, chooseActionFromState(currentState))
+        G += reward
+        if(not currentState): # if currentState is false (we know its the end of the episode)
+            return G
+
+
+def chooseActionFromState(S): # aka the policy
+    # 0 is to sick and 1 is to hit
+    # randomly choose one of those actions
+    return np.random.randint(0,2)  # will return ints between [0,2)
 
 
 run(10)
