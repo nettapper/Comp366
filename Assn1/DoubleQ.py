10d8774935992b5405b25033e48a9f1cca42c460
Conner D <connerdunn7399@gmail.com>
2016-10-31 22:27:56 -0600

    Ok I think we can learn now....

--- Assn1/DoubleQ.py
+++ Assn1/DoubleQ.py
@@ -16,20 +16,25 @@ def learnEpisode(alpha, eps, gamma):
         while(True):  # repeate for each step of the episode
             action = epsGreedyPolicy(currentState, eps)
             (reward, nextState) = blackjack.sample(currentState, action)
-            if(np.random.randint(0,2)):  # will return ints between [0,2)
-                Q1[currentState, action] = Q1[currentState, action] + alpha * ( reward + gamma * Q2[nextState, np.argmax(Q1[nextState])] - Q1[currentState, action])
-            else:
-                Q2[currentState, action] = Q2[currentState, action] + alpha * ( reward + gamma * Q1[nextState, np.argmax(Q2[nextState])] - Q2[currentState, action])
-            currentState = nextState
-            if(not currentState): # if currentState is false (we know its the end of the episode)
-                return
+            if(nextState):
+                if(np.random.randint(0,2)):  # will return ints between [0,2)
+                    Q1[currentState, action] = Q1[currentState, action] + alpha * ( reward + gamma * Q2[nextState, np.argmax(Q1[nextState])] - Q1[currentState, action])
+                else:
+                    Q2[currentState, action] = Q2[currentState, action] + alpha * ( reward + gamma * Q1[nextState, np.argmax(Q2[nextState])] - Q2[currentState, action])
+                currentState = nextState
+            else: # we know its the terminal state so the 'next rewards' simplify to 0 and can be ommited
+                if(np.random.randint(0,2)):  # will return ints between [0,2)
+                    Q1[currentState, action] = Q1[currentState, action] + alpha * ( reward - Q1[currentState, action])
+                else:
+                    Q2[currentState, action] = Q2[currentState, action] + alpha * ( reward - Q2[currentState, action])
+                return # if nextState is false (we know its the end of the episode)
 
 
 def evaluate(numEvaluationEpisodes):
     if(numEvaluationEpisodes > 0):
         returnSum = 0.0
         alpha = 0.001
-        eps = 1
+        eps = 0  # 0 is fully greedy, where as 1 is fully exploratory
         for episodeNum in range(numEvaluationEpisodes):
             G = episode(0, eps)
             returnSum = returnSum + G
@@ -62,8 +67,8 @@ def alwaysGreedyPolicy(currentState):  # given a state this will return an actio
 def run():
     alpha = 0.001
     eps = 1
-    numTrainingEpisodes = 100000
-    numEvaluationEpisodes = 10000
+    numTrainingEpisodes = 400000
+    numEvaluationEpisodes = 400000
     learn(alpha, eps, numTrainingEpisodes)
     print(Q1)
     print("Average:", evaluate(numEvaluationEpisodes))
