acf0d01cca75e0bbaa7fcff70f260902fc1c9f49
Conner D <connerdunn7399@gmail.com>
2016-10-31 20:32:42 -0600

    First part of eps greedy

--- Assn1/DoubleQ.py
+++ Assn1/DoubleQ.py
@@ -16,18 +16,27 @@ def evaluate(numEvaluationEpisodes):
     alpha = 0.001
     eps = 1
     for episodeNum in range(numEvaluationEpisodes):
-        G = episode(0)
+        G = episode(0, eps)
         returnSum = returnSum + G
     return returnSum/numEvaluationEpisodes
 
 
-def episode(G):
+def episode(G, eps):
     currentState = blackjack.init() # returns the initial state
     while(True):  # repeate for each step
-        (reward, currentState) = blackjack.sample(currentState, alwaysGreedyPolicy(currentState))
+        (reward, currentState) = blackjack.sample(currentState, epsGreedyPolicy(currentState, eps))
         G += reward
         if(not currentState): # if currentState is false (we know its the end of the episode)
+
             return G
+
+def epsGreedyPolicy(currentState, eps):  # given a state this will return an action
+    if(np.random.random() > eps):
+        combined = Q1[currentState] + Q2[currentState]
+        return ....
+    else:
+        return alwaysGreedyPolicy(currentState)
+
 
 def alwaysGreedyPolicy(currentState):  # given a state this will return an action
     combined = Q1[currentState] + Q2[currentState]
