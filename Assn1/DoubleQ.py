be189509be07736a83db4f5764fa6e71557dc929
Conner D <connerdunn7399@gmail.com>
2016-10-31 21:44:58 -0600

    I think that the learning is working

--- Assn1/DoubleQ.py
+++ Assn1/DoubleQ.py
@@ -7,18 +7,35 @@ Q2 = np.zeros((181,2)) # NumPy array of correct size
 
 
 def learn(alpha, eps, numTrainingEpisodes):
-    pass
-    # ... # Fill in Q1 and Q2
+    gamma = 1
+    for numTrainingEpisodes in range(numTrainingEpisodes):
+        learnEpisode(alpha, eps, gamma)
+
+def learnEpisode(alpha, eps, gamma):
+        currentState = blackjack.init() # returns the initial state
+        while(True):  # repeate for each step of the episode
+            action = epsGreedyPolicy(currentState, eps)
+            (reward, nextState) = blackjack.sample(currentState, action)
+            if(np.random.randint(0,2)):  # will return ints between [0,2)
+                Q1[currentState, action] = Q1[currentState, action] + alpha * ( reward + gamma * Q2[nextState, np.argmax(Q1[nextState])] - Q1[currentState, action])
+            else:
+                Q2[currentState, action] = Q2[currentState, action] + alpha * ( reward + gamma * Q1[nextState, np.argmax(Q2[nextState])] - Q2[currentState, action])
+            currentState = nextState
+            if(not currentState): # if currentState is false (we know its the end of the episode)
+                return
 
 
 def evaluate(numEvaluationEpisodes):
-    returnSum = 0.0
-    alpha = 0.001
-    eps = 1
-    for episodeNum in range(numEvaluationEpisodes):
-        G = episode(0, eps)
-        returnSum = returnSum + G
-    return returnSum/numEvaluationEpisodes
+    if(numEvaluationEpisodes > 0):
+        returnSum = 0.0
+        alpha = 0.001
+        eps = 1
+        for episodeNum in range(numEvaluationEpisodes):
+            G = episode(0, eps)
+            returnSum = returnSum + G
+        return returnSum/numEvaluationEpisodes
+    else:
+        return "Please call with a number of episodes greater than 0"
 
 
 def episode(G, eps):
@@ -39,9 +56,18 @@ def epsGreedyPolicy(currentState, eps):  # given a state this will return an act
 
 
 def alwaysGreedyPolicy(currentState):  # given a state this will return an action
-    combined = Q1[currentState] + Q2[currentState]
-    return np.argmax(combined)
+    return np.argmax(Q1[currentState] + Q2[currentState])
 
 
+def run():
+    alpha = 0.001
+    eps = 1
+    numTrainingEpisodes = 100000
+    numEvaluationEpisodes = 10000
+    learn(alpha, eps, numTrainingEpisodes)
+    print(Q1)
+    print("Average:", evaluate(numEvaluationEpisodes))
 
-print("Average:", evaluate(10000))
+
+run()
+
