f1286bfc9094ad63404ff9680c21f3124d078998
Conner D <connerdunn7399@gmail.com>
2016-11-14 19:15:48 -0700

    Ok, pretty sure this is working!

--- Assn2/Tilecoder.py
+++ Assn2/Tilecoder.py
@@ -1,9 +1,23 @@
 numTilings = 8
-    
+
 def tilecode(in1,in2,tileIndices):
-    # write your tilecoder here (5 lines or so)
-    
-    
+    for i in range(len(tileIndices)):
+        topRightOfZerothTile = 0.6 + (((-0.6) / 8) * i)
+        x = in1 // 0.6
+        y = in2 // 0.6
+        modx = in1 % 0.6
+        mody = in2 % 0.6
+        if(modx > topRightOfZerothTile):
+            x += 1
+        if(mody > topRightOfZerothTile):
+            y += 1
+        tileIndices[i] = linearize(x,y) + (121 * i)
+    return tileIndices
+
+def linearize(x,y):
+    return x + (y * 11)
+
+
 def printTileCoderIndices(in1,in2):
     tileIndices = [-1]*numTilings
     tilecode(in1,in2,tileIndices)
@@ -13,4 +27,4 @@ printTileCoderIndices(0.1,0.1)
 printTileCoderIndices(4.0,2.0)
 printTileCoderIndices(5.99,5.99)
 printTileCoderIndices(4.0,2.1)
-    
+
