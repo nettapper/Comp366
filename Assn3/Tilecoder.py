c9da2e71fc99e66c95d5d4cfe52a74b62fbb8619
Conner D <connerdunn7399@gmail.com>
2016-11-28 18:02:56 -0700

    more testing need but think it's working :camel:

--- Assn3/Tilecoder.py
+++ Assn3/Tilecoder.py
@@ -1,17 +1,27 @@
+placesToRoundTo = 6
 numTilings = 4
 minBound = -0.6
 maxBound = -0.4
+if(minBound < 0):
+    normalizedMinBound = minBound + abs(minBound)
+    normalizedMaxBound = maxBound + abs(minBound)
+else:
+    normalizedMinBound = minBound
+    normalizedMaxBound = maxBound
 widthOfTilingInTiles = 9
-widthOfTiling = (abs(maxBound - minBound)) / (widthOfTilingInTiles - 1)
+widthOfTiling = (normalizedMaxBound - normalizedMinBound) / (widthOfTilingInTiles - 1)
 
 
 def tilecode(in1, in2, tileIndices):
+    if(minBound < 0):
+        in1 = in1 + abs(minBound)
+        in2 = in2 + abs(minBound)
     for i in range(len(tileIndices)):
-        topRightOfZerothTile = widthOfTiling + (((-widthOfTiling) / numTilings) * i)
+        topRightOfZerothTile = round(widthOfTiling + (((-widthOfTiling) / numTilings) * i), placesToRoundTo)
         x = in1 // widthOfTiling
         y = in2 // widthOfTiling
-        modx = round(in1 % widthOfTiling, 6)
-        mody = round(in2 % widthOfTiling, 6)
+        modx = round(in1 % widthOfTiling, placesToRoundTo)
+        mody = round(in2 % widthOfTiling, placesToRoundTo)
         if(modx >= topRightOfZerothTile):
             x += 1
         if(mody >= topRightOfZerothTile):
@@ -30,7 +40,7 @@ def printTileCoderIndices(in1, in2):
     print('Tile indices for input (', in1, ',', in2, ') are : ', tileIndices)
 
 
-#printTileCoderIndices(-0.6, -0.6)
-#printTileCoderIndices(4.0, 2.0)
+printTileCoderIndices(-0.6, -0.6)
+printTileCoderIndices(-0.39, -0.39)
 #printTileCoderIndices(5.99, 5.99)
 #printTileCoderIndices(4.0, 2.1)
