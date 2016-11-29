a7f64130ab2de2d3d1d38e88de7a6fa91aa9a9aa
dhaberst <daniel.haberstock1@gmail.com>
2016-11-28 17:50:32 -0700

    Generalized TileCoder and changed globals :penguin:.

--- Assn3/Tilecoder.py
+++ Assn3/Tilecoder.py
@@ -1,23 +1,27 @@
-numTilings = 8
+numTilings = 4
+minBound = -0.6
+maxBound = -0.4
+widthOfTilingInTiles = 9
+widthOfTiling = (abs(maxBound - minBound)) / (widthOfTilingInTiles - 1)
 
 
 def tilecode(in1, in2, tileIndices):
     for i in range(len(tileIndices)):
-        topRightOfZerothTile = 0.6 + (((-0.6) / 8) * i)
-        x = in1 // 0.6
-        y = in2 // 0.6
-        modx = round(in1 % 0.6, 6)
-        mody = round(in2 % 0.6, 6)
+        topRightOfZerothTile = widthOfTiling + (((-widthOfTiling) / numTilings) * i)
+        x = in1 // widthOfTiling
+        y = in2 // widthOfTiling
+        modx = round(in1 % widthOfTiling, 6)
+        mody = round(in2 % widthOfTiling, 6)
         if(modx >= topRightOfZerothTile):
             x += 1
         if(mody >= topRightOfZerothTile):
             y += 1
-        tileIndices[i] = linearize(x, y) + (121 * i)
+        tileIndices[i] = linearize(x, y) + (widthOfTilingInTiles**2 * i)
     return tileIndices
 
 
 def linearize(x, y):
-    return int(x + (y * 11))
+    return int(x + (y * widthOfTilingInTiles))
 
 
 def printTileCoderIndices(in1, in2):
@@ -26,7 +30,7 @@ def printTileCoderIndices(in1, in2):
     print('Tile indices for input (', in1, ',', in2, ') are : ', tileIndices)
 
 
-# printTileCoderIndices(0.1, 0.1)
-# printTileCoderIndices(4.0, 2.0)
-# printTileCoderIndices(5.99, 5.99)
-# printTileCoderIndices(4.0, 2.1)
+#printTileCoderIndices(-0.6, -0.6)
+#printTileCoderIndices(4.0, 2.0)
+#printTileCoderIndices(5.99, 5.99)
+#printTileCoderIndices(4.0, 2.1)
